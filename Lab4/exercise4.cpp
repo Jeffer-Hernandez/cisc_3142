ab92d7d1b789a4bae4b2fda675ef26862d429c55
Jeff <jeffhernandez@nypl.org>
2022-09-28 19:53:20 -0400

    comments to ex4 and yaml change

--- Lab4/exercise4.cpp
+++ Lab4/exercise4.cpp
@@ -3,9 +3,24 @@ using namespace std;
 
 // main entry point
 int main() {
-    
+    // ask for the cost of an item 
+    // assign to a "cost" variable of type float
 
-    
+    // ask for the number of years from now the item will be purchased 
+    // assign the "years" variable of integer type
+
+    // ask for the rate of inflation
+    // assign to a "rate" variable of type float
+
+    // divide the "rate" variable by 100 to create the percentage decimal version
+
+    // assign "final_price" variable of type float
+    // the initial value of the "final_price" variable is the 
+        // product of the cost variable and the rate variable
+
+    // for loop through all the years provided by the user
+    // for each loop, final_price += (final_price * rate)
+    // return final_price
 
     return 0;
 }
