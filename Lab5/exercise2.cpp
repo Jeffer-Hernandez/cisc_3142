f7005d5c5578adddbf0305a31059fe2dbed4cebc
Jeff <jeffhernandez@nypl.org>
2022-10-13 19:12:06 -0400

    finished exercsie2

--- Lab5/exercise2.cpp
+++ Lab5/exercise2.cpp
@@ -9,21 +9,35 @@ using namespace std;
 
 */
 
-int main(){
-
-    // declare variables, booleans
-    int i, n;
-
-    // loop to 100 to check to find prime numbers
-    for (i = 3; i <= 100; ++i) {
-
-        // some condition to find prime numbers
-        if (i ) {
-        // print the numbers
-
-        }
+// function to check if a given number is prime
+bool isPrime(int n)
+{
+    // since 0 and 1 is not prime return false.
+    if (n == 1 || n == 0)
+        return false;
+  
+    // Run a loop from 2 to n-1
+    for (int i = 2; i < n; i++) {
+        // if the number is divisible by i, then n is not a
+        // prime number.
+        if (n % i == 0)
+            return false;
     }
-
-
+    // otherwise, n is prime number.
+    return true;
+}
+  
+// main entry point
+int main()
+{
+    int n = 100;
+  
+    // check for every number from 1 to N
+    for (int i = 1; i <= n; i++) {
+        // check if current number is prime
+        if (isPrime(i))
+            cout << i << " ";
+    }
+  
     return 0;
 }
