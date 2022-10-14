73da684dcfdeeb050748e49d6ef082199a0e2222
Jeff <jeffhernandez@nypl.org>
2022-10-13 21:22:00 -0400

    created data.txt and finished ex 3

--- Lab5/exercise3.cpp
+++ Lab5/exercise3.cpp
@@ -1,26 +1,57 @@
 #include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
 using namespace std;
 
 /*
 
-    Write a program that reads a file containing a pair of numbers in each line 
+    Write a program that reads a file containing a pair of numbers in each line
     (representing x y coordinates); at the end of reading this file compute
     the linear regression for this set of numbers. Please use to the
-    linear regression formulas mentioned in class (if you missed class, find a 
+    linear regression formulas mentioned in class (if you missed class, find a
     classmate to get notes).
 
 */
 
-int main(){
-    
+double slope(const vector<double> &x, const vector<double> &y)
+{
+    double n = x.size();
+    double s_x = accumulate(x.begin(), x.end(), 0.0);
+    double s_y = accumulate(y.begin(), y.end(), 0.0);
+    double s_xx = inner_product(x.begin(), x.end(), x.begin(), 0.0);
+    double s_xy = inner_product(x.begin(), x.end(), y.begin(), 0.0);
+    double a = (n * s_xy - s_x * s_y) / (n * s_xx - s_x * s_x);
+    return a;
+}
+
+int main()
+{
+    // declare variables, vectors
+    int i;
+    vector<double> x(6);
+    vector<double> y(6);
+
     // read from file
+    ifstream inFile;
+    inFile.open("data.txt");
+
+    if (!inFile)
+    {
+        cerr << "Unable to open file data.txt";
+        exit(1); // call system to stop
+    }
 
     // loop through the file line by line
+    while (inFile >> i)
+    {
+        inFile >> y[i];
+        inFile >> x[i];
+    }
 
-    // for each line, declare x and y variables for the values
+    cout << slope(x, y) << '\n';
 
-    // use formulas to calculate linear regression
-
-
+    inFile.close();
     return 0;
 }
